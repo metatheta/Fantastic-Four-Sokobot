efa726ed97caa788d82d2abb6e2ab9a5b63418fc
Miguel <mlpgmnrdps@gmail.com>
2025-09-16 00:24:09 +0800

    feat(Test1.py): asks for 2 numbers and prints product

---
+++ Test1.py
@@ -0,0 +1,9 @@
+"""
+Testing this file by making 2 versions of the same file
+that will conflict upon merge
+"""
+
+x = int(input("Give a number: "))
+y = int(input("Give another number: "))
+
+print(f"The product of the numbers is {x * y}")
