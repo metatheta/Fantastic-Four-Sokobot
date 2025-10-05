928c15d5164312ad1de2feee9949fac06c10ea8d
nathan <natetrndd@gmail.com>
2025-10-06 03:15:33 +0800

    refactor: rename x/y fields to row/col and rewrite equals()

--- Sirs-Sokobot/src/solver/Coords.java
+++ Sirs-Sokobot/src/solver/Coords.java
@@ -1,36 +1,43 @@
 package solver;
 
 public class Coords implements Comparable<Coords>{
-    
-    public final int x;
-    public final int y;
-    
-    public Coords(int x, int y)
+
+    public final int row;
+    public final int col;
+
+    public Coords(int row, int col)
     {
-        this.x = x;
-        this.y = y;
+        this.row = row;
+        this.col = col;
     }
 
     @Override
     public boolean equals(Object o)
     {
-        if (!(o instanceof Coords c))
+        if (o == null)
             return false;
-        return c.x == x && c.y == y;
+        if (o == this)
+            return true;
+        if (this.getClass() != o.getClass())
+            return false;
+        Coords c = (Coords) o;
+        if(this.hashCode() == c.hashCode())
+            return true;
+        return c.row == row && c.col == col;
     }
 
      @Override
-    public int compareTo(Coords other) 
+    public int compareTo(Coords other)
     {
-        if (this.x != other.x) 
-            return this.x - other.x;
-        return this.y - other.y;                        
+        if (this.row != other.row)
+            return this.row - other.row;
+        return this.col - other.col;
     }
 
     @Override
-    public int hashCode() 
+    public int hashCode()
     {
-        return 31 * x + y;
+        return 31 * row + col;
     }
 
 }
