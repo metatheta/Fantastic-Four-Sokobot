4357fda0b9c14425db7b3748ec54a591028a9f69
Stephen <stephen.jszs.gplay@gmail.com>
2025-10-22 01:38:47 +0800

    Improved heuristic search function to be 10x faster.

--- Sirs-Sokobot/src/solver/Heuristic.java
+++ Sirs-Sokobot/src/solver/Heuristic.java
@@ -13,102 +13,45 @@ public class Heuristic {
 		this.boxValues = new HashMap<Integer, Integer>(); 
 	}
 
+    /**
+     * Heuristic computation method. State is based on the sum of total manhattan distance
+     * from box to nearest goal + total manhattan distance from player to a box
+     * This also stores the values in a dedicated hashmap
+     * @param state current state
+     * @return heuristic value
+     */
 	public int calculateHeuristic(State state) {
 		if (stateValues.containsKey(state.hashCode()))
 			return stateValues.get(state.hashCode());
 
-        //Compute heuristic value
-		int value = distancePlayerBox(state) + manhattanBoxGoal(state);
-        //value += distancePlayerBox(state); //Search closest box + manhattan distance to a goal state
-        // if (state.pushState)
-        //value += manhattanBoxGoal(state); //When pushing the box compute total manhattan distance
+        int boxGoalDistance, playerBoxDistance;
+        int goalBoxMin, playerBoxMin;
+        int value = 0;
+        for (Coords box : state.boxes) {
+            playerBoxMin = goalBoxMin = 99999;
+            //compute minimum manhattan distance from box to nearest goal
+            if (boxValues.containsKey(box.hashCode())) {
+                goalBoxMin = boxValues.get(box.hashCode());
+            } else {
+                //Go through each goal
+                for (Coords goal : board.goals) {
+                    boxGoalDistance = Math.abs(goal.col - box.col) + Math.abs(goal.row - box.row);
+                    if (boxGoalDistance < goalBoxMin) {
+                        goalBoxMin = boxGoalDistance;
+                    }
+                }
+                boxValues.put(box.hashCode(), goalBoxMin);
+            }
 
+            //Compute manhattan distance from player to box
+            playerBoxDistance = Math.abs(state.player.col - box.col)
+                    + Math.abs(state.player.row - box.row);
+            if (playerBoxDistance < playerBoxMin) {
+                playerBoxMin = playerBoxDistance;
+            }
+            value += goalBoxMin + playerBoxMin;
+        }
 		stateValues.put(state.hashCode(), value);
 		return value;
 	}
-
-    /**
-     * Compares manhattan distance of all boxes to the closest goal and takes the minimum
-     * @param state the current state
-     * @return the smallest minimum manhattan distance among all goals
-     */
-    private int manhattanBoxGoal(State state) {
-        int value = 0;
-
-        //Gets the manhattan distance of box to goal
-        for (Coords box : state.boxes) {
-            // Compare to positions of other boxes
-
-            int minimumDistance = 99999;
-
-            if (boxValues.containsKey(box.hashCode())) {
-                minimumDistance = boxValues.get(box.hashCode());
-            } else {
-                for (Coords goal : board.goals) {
-                    // Compute Manhattan distance
-
-                    int distance =
-                            (int)Math.abs(goal.col - box.col)
-                                    + (int)Math.abs(goal.row - box.row);
-
-                    if (distance < minimumDistance) {
-                        minimumDistance = distance;
-                    }
-
-                }
-                boxValues.put(box.hashCode(), minimumDistance);
-            }
-
-            value += minimumDistance;
-        }
-
-        return value;
-    }
-
-    /**
-     * Gets minimum euclideanish distance from player to closest box and manhattan distance of that box
-     * to the closest goal
-     * @param state the current state
-     * @return manhattan heuristic idk player -> box -> goal
-     */
-    private int distancePlayerBox(State state) {
-        int value = 0;
-        int manhattan = 0;
-        boolean isGoal = false;
-
-        //Gets the manhattan distance of player to closest box
-        for (Coords box : state.boxes) {
-            // Compare to positions of other boxes
-            for (Coords goal : board.goals) {
-                if (box.row == goal.row && box.col == goal.col) {
-                    isGoal = true;
-                    break;
-                } else
-                    manhattan = (int)Math.abs(goal.col - box.col)
-                            + (int)Math.abs(goal.row - box.row);
-            }
-
-            if (isGoal)
-                continue;
-
-            int minimumDistance = 99999;
-            if (boxValues.containsKey(box.hashCode())) {
-                minimumDistance = boxValues.get(box.hashCode());
-            } else {
-                int distance =
-                        (int) Math.sqrt((int)Math.abs(state.player.col - box.col)
-                                + (int)Math.abs(state.player.row - box.row));
-
-                if (distance < minimumDistance) {
-                    minimumDistance = distance;
-                }
-            }
-
-            //if (value == 0 || minimumDistance < value)
-                value += minimumDistance;
-        }
-        //value = 0;
-
-        return value + manhattan;
-    }
 }
